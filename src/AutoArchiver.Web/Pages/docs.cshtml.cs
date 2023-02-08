88acb625175d82bc273661bf929480eb4af8d81b
Dave Mateer <davemateer@gmail.com>
2023-02-08 16:30:27 +0000

    auto

---
+++ src/AutoArchiver.Web/Pages/docs.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AutoArchiver.Web.Pages
+{
+    public class DocsModel : PageModel
+    {
+        public string? CacheBust { get; set; }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public DocsModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+    }
+}
