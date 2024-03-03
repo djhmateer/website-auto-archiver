e6cba01279b80a3bda082973f16131334c40b788
Dave Mateer <davemateer@gmail.com>
2024-03-03 15:58:50 +0000

    auto

--- src/AutoArchiver.Web/Pages/Index.cshtml.cs
+++ src/AutoArchiver.Web/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Serilog;
 
 namespace AutoArchiver.Web.Pages
 {
@@ -21,44 +22,43 @@ namespace AutoArchiver.Web.Pages
 
         public void OnGet()
         {
-
-        }
+        } 
 
         //public async Task<IActionResult> OnPost(string email, string message)
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> OnPost(string? email, string? message)
-        {
-            // could do a lot of modelstate validation here
-            if (string.IsNullOrEmpty(email))
-                return LocalRedirect("/email-fail");
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> OnPost(string? email, string? message)
+        //{
+        //    // could do a lot of modelstate validation here
+        //    if (string.IsNullOrEmpty(email))
+        //        return LocalRedirect("/email-fail");
 
-            var postmarkServerToken = AppConfiguration.LoadFromEnvironment().PostmarkServerToken;
+        //    var postmarkServerToken = AppConfiguration.LoadFromEnvironment().PostmarkServerToken;
 
-            //var response = await Email.SendTemplate("forgot-password", email, "Thank you - we will get back to you!", postmarkServerToken);
+        //    //var response = await Email.SendTemplate("forgot-password", email, "Thank you - we will get back to you!", postmarkServerToken);
 
-            // send confirmation to the person who just submitted email on site
-            {
-                var ToEmailAddress = email;
-                var subject = "Thank you";
-                var text = "Thank you - we will get back to you soon";
-                var html = "Thank you - we will get back to you soon";
-                var foo = new AAEmail(ToEmailAddress, subject, text, html);
-                var response = await Email.Send(foo, postmarkServerToken);
-            }
+        //    // send confirmation to the person who just submitted email on site
+        //    {
+        //        var ToEmailAddress = email;
+        //        var subject = "Thank you";
+        //        var text = "Thank you - we will get back to you soon";
+        //        var html = "Thank you - we will get back to you soon";
+        //        var foo = new AAEmail(ToEmailAddress, subject, text, html);
+        //        var response = await Email.Send(foo, postmarkServerToken);
+        //    }
 
-            // send email to the admin 
-            {
-                var ToEmailAddress = "dave@hmsoftware.co.uk";
-                var subject = "auto-archiver message";
-                var text = $"sender is: {email}, message is: {message}";
-                var html = $"sender is: {email}, message is: {message}";
-                var foo = new AAEmail(ToEmailAddress, subject, text, html);
-                var response = await Email.Send(foo, postmarkServerToken);
-            }
+        //    // send email to the admin 
+        //    {
+        //        var ToEmailAddress = "dave@hmsoftware.co.uk";
+        //        var subject = "auto-archiver message";
+        //        var text = $"sender is: {email}, message is: {message}";
+        //        var html = $"sender is: {email}, message is: {message}";
+        //        var foo = new AAEmail(ToEmailAddress, subject, text, html);
+        //        var response = await Email.Send(foo, postmarkServerToken);
+        //    }
 
-            // PRG
-            return LocalRedirect("/email-success");
-        }
+        //    // PRG
+        //    return LocalRedirect("/email-success");
+        //}
     }
 }
